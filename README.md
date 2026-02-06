d043edfdb974bfca1f7d93109b21ad34028c7e62
alvarogimenezc <alvarogimenezc24@gmail.com>
2026-02-06 16:02:31 +0100

    Add /ingestion route and update README
    Add a POST /ingestion route skeleton to servidor.py (ruta1) and perform minor whitespace cleanup. Update README.md to describe the ingestion script: it generates a random JSON purchase ticket with predefined values and posts it to the Flask server.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ingestion
+
+Este script genera un json aleatorio con un ticket de compra (con una serie de valores predefinidos). Después pasamos el json a el servidor de flask.
--- servidor.py
+++ servidor.py
@@ -1,10 +1,14 @@
 import flask 
 
-
 #Creamos la instancia de aplicación (levantamos el servidor)
 app = flask.Flask(__name__)
 
 #Ruta1
+@app.route('/ingestion', methods=['POST'])
+def ruta1():
+    
+
+
 #Ruta2
 #Ruta3
 #Ruta4
